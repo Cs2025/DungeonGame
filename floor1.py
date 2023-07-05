ad656782ba79ab46e6b76d8ff6825834540c3fb6
cshenton42 <cshenton42@tntech.edu>
2023-07-05 03:35:39 -0500

    Added choices for going through left door in floor1.py Updated TODO

--- floor1.py
+++ floor1.py
@@ -12,3 +12,51 @@ def choice4_option_1(self):
     print("\nPress any key to continue...")
     msvcrt.getch()
     os.system("cls")
+
+    while True:
+        print("What will you do now?\n")
+        if self.character.has_torch:
+            print("1. Jump over the hole")
+            print("2. Maneuver past the hole")
+            print("3. \x1b[32mAttempt to burn roots\x1b[0m")
+            print("I. Show inventory")
+        else:
+            print("1. Jump over the hole")
+            print("2. Maneuver past the hole")
+            print("I. Show inventory")
+
+        choice4_1 = input("\nEnter your choice: ")
+
+        if choice4_1 == "1":
+            os.system("cls")
+            self.type_text_slowly("You proceed through the left door...\n", 0.001)
+            print("\nPress any key to continue...")
+            msvcrt.getch()
+            os.system("cls")
+            break
+
+        elif choice4_1 == "2":
+            os.system("cls")
+            self.type_text_slowly("You proceed through the center door...\n", 0.001)
+            print("\nPress any key to continue...")
+            msvcrt.getch()
+            os.system("cls")
+            break
+        elif choice4_1 == "3":
+            os.system("cls")
+            self.type_text_slowly("You proceed through the right door...\n", 0.001)
+            print("\nPress any key to continue...")
+            msvcrt.getch()
+            os.system("cls")
+            break
+        elif choice4_1.lower() == "i":
+            os.system("cls")
+            print("Inventory:")
+            for item in self.character.get_inventory():
+                print(f"- {item}")
+            print("Coins: " + str(self.character.coins))
+            print("\nPress any key to continue...")
+            msvcrt.getch()
+            os.system("cls")
+        else:
+            print("Invalid choice. Please try again.\n")
